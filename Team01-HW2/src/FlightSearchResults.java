550ed36840f80afae2d081e0eefceac615c54448
Cbodfield <cbodfield@gmail.com>
2014-10-16 20:13:30 -0500

    Built SearchResults query
    Also removed jdbchelper jar and added java-json jar

--- Team01-HW2/src/FlightSearchResults.java
+++ Team01-HW2/src/FlightSearchResults.java
@@ -1,37 +1,72 @@
-
-
 import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import classes.JDBCHelper;
+import classes.ResultSetConverter;
 
 /**
  * Servlet implementation class FlightSearchResults
  */
 public class FlightSearchResults extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public FlightSearchResults() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public FlightSearchResults() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+	}
+
+	public JSONArray getFlightDetails(String sFlightNumber) {
+		ArrayList<Object> sqlParam = new ArrayList<Object>();
+		sqlParam.add(sFlightNumber);
+
+		JDBCHelper jdbc = new JDBCHelper();
+		jdbc.connectToCSE464DB();
+		String query = "SELECT * FROM flights JOIN planes ON flights.plane = planes.id WHERE flights.id = ?;";
+
+		ResultSet rs = jdbc.queryDB(query, sqlParam);
+		JSONArray results = new JSONArray();
+		try {
+			results = ResultSetConverter.convert(rs);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (JSONException e) {
+			e.printStackTrace();
+		} finally {
+			jdbc.closeConnection();
+		}
+
+		return results;
 	}
 
 }
